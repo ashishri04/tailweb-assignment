df3e86112f49babfa7aaeeb47658ccdbbd14955f
ashishri04 <ashi.shri42@gmail.com>
2023-01-13 22:33:30 +0530

    added

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express= require("express")
+const bodyParser = require ("body-parser")
+const {default:mongoose} = require ("mongoose")
+const router = require("./routes/route")
+const app = express()
+
+
+app.use(express.json())
+
+
+mongoose.connect("mongodb+srv://ashi:jhansi284205@myfirstcluster.tfihevu.mongodb.net/project1",{ 
+   useNewUrlParser: true})
+.then( ()=> console.log("mongodb is connected"))
+.catch(err => console.log(err))
+
+app.use('/',router)
+
+app.listen(process.env.PORT || 3000, function(){
+   console.log('express app running on PORT'+ (process.env.PORT || 3000))
+})
